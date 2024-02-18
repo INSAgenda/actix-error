726aff886b93651a041cb192010f658f7be22507
DimitriTimoz <dimitri.timoz@protonmail.com>
2024-02-18 22:29:38 +0100

    Remove translation

--- actix-error-derive/src/lib.rs
+++ actix-error-derive/src/lib.rs
@@ -1,61 +1,17 @@
 use darling::FromVariant;
-use std::{path::PathBuf, collections::HashMap};
-
 use syn::{parse_macro_input, DeriveInput};
 
-use proc_macro2::TokenTree;
 use proc_macro::TokenStream;
-
-#[cfg(feature = "json")]
-mod json;
-#[cfg(feature = "json")]
-use crate::json::get_json_error_messages;
-
-#[cfg(feature = "po")]
-use crate::po::get_po_error_messages;
-#[cfg(feature = "po")]
-mod po;
-
 /// 
 #[derive(FromVariant, Default)]
 #[darling(default, attributes(error))]
 struct Opts {
     code: Option<u16>,
-    msg_id: Option<String>,
-    kind: Option<String>,
     status: Option<String>,
+    kind: Option<String>,
+    msg: Option<String>,
 }
 
-
-#[cfg(any(feature = "po", feature = "json"))]
-fn get_dir_attr(attrs: &Vec<syn::Attribute>, attr_name: &str) -> Option<PathBuf> {
-    let mut directory_tokens = attrs.iter().find(|attr| attr.path.is_ident(attr_name)).expect("Couldn't get the attribute").tokens.clone().into_iter();
-    match directory_tokens.next() {
-        Some(TokenTree::Punct(punct)) if punct.as_char() == '=' => (),
-        _ => panic!("Expected leading '=' in {attr_name} attribute"),
-    }
-    let directory = match directory_tokens.next() {
-        Some(TokenTree::Literal(value)) => value.to_string(),
-        _ => panic!("Expected literal in {attr_name} attribute")
-    };
-    let directory = directory.trim_matches('"');
-    
-    // Check if the directory exists and contains at least one .po file.
-    let directory = std::path::PathBuf::from(directory);
-    if !directory.exists() {
-        panic!("The {attr_name} directory does not exist");
-    }
-    if !directory.is_dir() {
-        panic!("The path {attr_name} is not a directory");
-    }
-    
-    let mut files = std::fs::read_dir(&directory).expect("Couldn't read the directory");
-    if files.next().is_none() {
-        panic!("The path {attr_name} does not contain any files");
-    }
-
-    Some(directory)
-}
 
 /// This derive macro is used to convert an enum into an ApiError.  
 /// You can use it by adding the ```#[derive(AsApiError)]``` attribute to your enum.  
@@ -87,44 +43,13 @@ fn get_dir_attr(attrs: &Vec<syn::Attribute>, attr_name: &str) -> Option<PathBuf>
 ///     _ => unreachable!(),
 /// }
 /// ```
-#[cfg_attr(all(feature = "json", feature="po"), proc_macro_derive(AsApiError, attributes(error, msg_path)))]
-#[cfg_attr(all(feature = "json", not(feature = "po")), proc_macro_derive(AsApiError, attributes(json_file, error)))]
-#[cfg_attr(all(feature = "po", not(feature = "json")), proc_macro_derive(AsApiError, attributes(po_directory, error)))]
-#[cfg_attr(not(any(feature = "po", feature = "json")), proc_macro_derive(AsApiError, attributes(error)))]
+#[proc_macro_derive(AsApiError, attributes(error))]
 pub fn derive(input: TokenStream) -> TokenStream {
     use convert_case::{Case, Casing};
 
     // Parse the input tokens into a syntax tree
     let ast = parse_macro_input!(input as DeriveInput); 
     let ident_name = ast.ident;
-
-    // Get the path to the po file
-    #[cfg(all(feature = "po", not(feature = "json")))]
-    let po_directory = get_dir_attr(&ast.attrs, "po_directory").expect("No po_directory attribute found");
-    
-    // Get the path to the json file
-    #[cfg(all(feature = "json", not(feature = "po")))]
-    let json_file = get_dir_attr(&ast.attrs, "json_file").expect("No json_file attribute found");
-
-    #[cfg(all(feature = "json", feature = "po"))]
-    let messages_catalog = {
-        let path = get_dir_attr(&ast.attrs, "msg_path").expect("No path attribute found");
-        // Check if the path is a directory
-        if path.is_dir() {
-            get_po_error_messages(path)
-        } else {
-            get_json_error_messages(path)
-        }
-    };
-    
-    #[cfg(all(feature = "json", not(feature = "po")))]
-    let messages_catalog = get_json_error_messages(json_file);
-
-    #[cfg(all(feature = "po", not(feature = "json")))]
-    let messages_catalog = get_po_error_messages(po_directory);
-
-    #[cfg(not(any(feature = "json", feature = "po")))]
-    let messages_catalog: HashMap<String, HashMap<String, String>> = HashMap::new();
 
     // Get the variants
     let enum_data = match ast.data {
@@ -136,6 +61,27 @@ pub fn derive(input: TokenStream) -> TokenStream {
     // Generate the variant's code 
     let variants = variants.iter().map(|v| {
         let ident = &v.ident;
+        let matching_wrapped = if let syn::Fields::Unnamed(u) = &v.fields {
+            let mut fields = String::new();
+            for (i, _) in u.unnamed.iter().enumerate() {
+                fields.push_str(&format!("a{}", i));
+                if i < u.unnamed.len() - 1 {
+                    fields.push_str(", ");
+                }
+            }
+            format!("({})", fields)
+        } else if let syn::Fields::Named(u) = &v.fields {
+            let mut fields = String::new();
+            for (i, field) in u.named.iter().enumerate() {
+                fields.push_str(field.ident.as_ref().unwrap().to_string().as_str());
+                if i < u.named.len() - 1 {
+                    fields.push_str(", ");
+                }
+            }
+            format!("{{ {} }}", fields)
+        } else {
+            String::new()
+        };
         // Get the tuple if it exists
         let tuple = match &v.fields {
             syn::Fields::Unnamed(u) => Some(u),
@@ -176,62 +122,37 @@ pub fn derive(input: TokenStream) -> TokenStream {
             }
         };
 
-        #[cfg(feature = "actix")]
-        {
-            use actix_web::http::StatusCode;
-            if let Err(e) = StatusCode::from_u16(code) {
-                panic!("Invalid status code for variant {}: {}", ident, e);
-            }
+        
+        use actix_web::http::StatusCode;
+        if let Err(e) = StatusCode::from_u16(code) {
+            panic!("Invalid status code for variant {}: {}", ident, e);
         }
+        let kind = opts.kind.unwrap_or_else(|| ident.to_string().to_case(Case::Snake));
+
         // Get the messages for the variant
-        let msg_id = opts.msg_id.unwrap_or_else(|| ident.to_string().to_case(Case::Snake));
-        let mut messages = String::new();
+        let mut message = "String::new()".to_owned();
+        if let Some(msg) = opts.msg {
+            message = if let Some(tuple) = tuple  {
+                // genrate a string like "format!(\"message\", self.0, self.1)"
+                // Where message is the msg attribute of the variant
+                // and self.0, self.1 are the tuple fields
+                let mut fields = String::new();
+                for (i, _) in tuple.unnamed.iter().enumerate() {
+                    fields.push_str(&format!("a{}", i));
+                    if i < tuple.unnamed.len() - 1 {
+                        fields.push_str(", ");
+                    }
+                }
+                format!("format!(\"{}\", {})", msg, fields)
+            } else if let Some(_) = struc {
+                format!("format!(\"{}\")", msg)
+            } else {
+                "String::new()".to_string()
+            };
+        }
+
         let mut list_vars = String::new();
         
-        // Add the default messages for the variant in a hashmap
-        for (k, v) in messages_catalog.get(&msg_id).expect(&format!("Couldn't get the messages for the variant \"{msg_id}\"")) {
-            list_vars = String::new();
-            let mut v = v.to_string();
-            if let Some(tuple) = tuple {
-                // Get the variables names and their calls
-                let tup: (Vec<String>, Vec<String>)= tuple.unnamed.iter().enumerate().map(|(i, field)| {
-                    if field.ty == syn::parse_str("Translation").unwrap() {
-                        (format!("a{i}"), format!("a{i}.get(\"{k}\")"))
-                    } else {
-                        (format!("a{i}"), format!("a{i}"))
-                    }
-                }).unzip();
-                // Get the variables names
-                list_vars = tup.0.join(", ");
-                // Get the variables calls
-                let list_calls = tup.1.join(", ");
-                // Count the number of "{}" in the message and compare it to the number of variables in the tuple
-                let nb = v.matches("{}").count();
-                if nb != tuple.unnamed.len() {
-                    panic!("The number of variables in the message for the variant \"{msg_id}\" must be equal to the number of variables in the tuple");
-                }
-                messages.push_str(
-                    &format!("(String::from(\"{k}\"), format!(\"{v}\", {list_calls})),")
-                );
-            } else if let Some(struc) = struc {
-                let vars = v.split("{").skip(1).map(|s| s.split("}").next().unwrap().to_string()).collect::<Vec<String>>();
-                let vars = vars.as_slice();
-                // Replace all the variabels in the message
-                for var in vars {
-                    v = v.replace(&format!("{{{}}}", var.clone()), "{}").to_string();
-                }
-                list_vars = struc.named.iter().map(|f| f.ident.as_ref().unwrap().to_string()).collect::<Vec<String>>().join(", ");
-                messages.push_str(
-                    &format!("(String::from(\"{k}\"), format!(\"{v}\", {list_vars})),")
-                );
-            } else {
-                messages.push_str(
-                    &format!("(String::from(\"{k}\"), String::from(\"{v}\")),")
-                );
-            }
-        }
-        // Get the kind of the variant
-        let kind = opts.kind.unwrap_or_else(|| ident.to_string().to_case(Case::Snake));
         // Add the tuple syntax if it exists
         if list_vars.len() > 0 {
             if struc.is_some() {
@@ -240,20 +161,23 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 list_vars = format!("( {} )", list_vars);
             }
         }
+
+        
         format!("
-            {ident_name}::{ident} {list_vars} => {{
+            {ident_name}::{ident} {matching_wrapped} {list_vars} => {{
                 ApiError::new(
                     {code}, 
                     \"{kind}\",
-                    HashMap::from([{messages}]), 
+                    {message}
                 )
             }},
-        ")
+        ", )
     });
 
     // Implement the ApiError trait
     let mut code = String::new();
-    code.push_str(&format!("impl AsApiError for {ident_name} {{\n"));
+    code.push_str("use actix_error::{AsApiErrorTrait, ApiError};\n");
+    code.push_str(&format!("impl AsApiErrorTrait for {ident_name} {{\n"));
     code.push_str(" fn as_api_error(&self) -> ApiError {\n");
     code.push_str("     match &self {\n");
     for v in variants {
@@ -263,7 +187,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     code.push_str("   }\n");
     code.push_str("}\n");
 
-    #[cfg(feature = "verbose")]
+    
     println!("code : {code}");
 
     code.parse().expect("Couldn't parse the code")
--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,33 @@
-use std::collections::HashMap;
 use actix_web::http::StatusCode;
-
-pub use actix_error_derive::AsApiError;
 use serde::Serialize;
-
-pub mod translate;
+pub use actix_error_derive::AsApiError;
 
 #[derive(Debug, Clone, Serialize)]
 pub struct ApiError {
     pub kind: String,
-    #[serde(skip_serializing)]
     pub code: u16,
-    #[serde(skip_serializing)]
-    pub messages: HashMap<String, String>,
-    message_fr: String,
-    message_en: String,
-    origin: String,
+    pub message: String,
 }
 
 impl ApiError {
-    pub fn new(code: u16, kind: &str, messages: HashMap<String, String>) -> Self {
-        let message_en = messages.get("en").unwrap_or(&String::new()).to_string();
-        let message_fr = messages.get("fr").unwrap_or(&String::new()).to_string();
+    pub fn new(code: u16, kind: &str, message: String) -> Self {
         Self {
             kind: kind.to_string(),
+            message,
             code,
-            messages,
-            message_fr,
-            message_en,
-            origin: String::new(),
         }
     }
 }
 
-pub trait AsApiError {
+pub trait AsApiErrorTrait {
     fn as_api_error(&self) -> ApiError;
 }
 
 impl std::fmt::Display for ApiError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{} : {:?}", self.kind, self.messages)
+        write!(f, "{} : {}", self.kind, self.message)
     }
 }
-#[cfg(feature = "po")]
-use translate::Translation;
 
 impl actix_web::ResponseError for ApiError {
     fn status_code(&self) -> StatusCode {
@@ -53,49 +37,4 @@ impl actix_web::ResponseError for ApiError {
     fn error_response(&self) -> actix_web::HttpResponse {
         actix_web::HttpResponse::build(self.status_code()).json(self)
     }
-}
-#[cfg(feature = "po")]
-#[derive(AsApiError)]
-#[cfg_attr(all(feature = "json", feature = "po"), msg_path = "locales/")]
-#[cfg_attr(all(not(feature = "json"), feature = "po"), po_directory = "locales/")]
-pub enum ErrorEn {
-    #[error(status = "BadRequest", msg_id = "invalid_password")]
-    InvalidPassword,
-    #[error(code = 404, msg_id = "invalid_id")]
-    InvalidId(u32),
-    #[error(code = 500, msg_id = "named_error")]
-    NamedError { name: String, age: u32 },
-    #[error(code = 500)]
-    NamedError2(Translation),
-}
-
-#[test]
-#[cfg(all(feature = "po", feature = "actix"))]
-fn default() {
-    let e = ErrorEn::InvalidPassword;
-    println!(
-        "Error::as_api_error() = {:?}",
-        serde_json::to_string(&e.as_api_error()).unwrap()
-    );
-    let e = ErrorEn::InvalidId(42);
-    println!(
-        "Error::as_api_error() = {:?}",
-        serde_json::to_string(&e.as_api_error()).unwrap()
-    );
-    let e = ErrorEn::NamedError {
-        name: "John".to_string(),
-        age: 42,
-    };
-    println!(
-        "Error::as_api_error() = {:?}",
-        serde_json::to_string(&e.as_api_error()).unwrap()
-    );
-    let e = ErrorEn::NamedError2(tr! {
-        "en" => "Hello",
-        "fr" => "Bonjour",
-    });
-    println!(
-        "Error::as_api_error() = {:?}",
-        serde_json::to_string(&e.as_api_error()).unwrap()
-    );
 }
