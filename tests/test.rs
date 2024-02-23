8a65cdfc73f182f3322aa0eca4c13a3a9bf39eb8
Dimitri <dimitri.timoz@orange.fr>
2024-02-23 12:35:13 +0100

    Group errors

--- tests/test.rs
+++ tests/test.rs
@@ -1,8 +1,16 @@
-use actix_error::AsApiError;
+use actix_error::*;
+
+#[derive(AsApiError)]
+pub enum GrpError {
+    #[error(status = "InternalServerError", msg = "Firewall fail")]
+    FirewallFail,
+    #[error(status = "BadRequest", msg = "Invalid token")]
+    InvalidToken,
+}
 
 #[derive(AsApiError)]
 pub enum ErrorEn {
-    #[error(status = "BadRequest", msg = "invalid_password")]
+    #[error(status = "BadRequest", msg = "Invalid password")]
     InvalidPassword,
     #[error(code = 404, msg = "invalid id {}")]
     InvalidId(u32),
@@ -10,6 +18,8 @@ pub enum ErrorEn {
     NamedError { name: String, age: u32 },
     #[error(status = "InternalServerError", msg = "Internal database error", ignore)]
     PostgresError(String),
+    #[error(group)]
+    GroupError(GrpError),
 }
 
 #[actix_web::test]
@@ -18,7 +28,7 @@ async fn test_error() {
     let api_error = error.as_api_error();
     assert_eq!(api_error.code, 400);
     assert_eq!(api_error.kind, "invalid_password");
-    assert_eq!(api_error.message, "invalid_password");
+    assert_eq!(api_error.message, "Invalid password");
 
     let error = ErrorEn::InvalidId(100);
     let api_error = error.as_api_error();
@@ -40,4 +50,10 @@ async fn test_error() {
     assert_eq!(api_error.code, 500);
     assert_eq!(api_error.kind, "postgres_error");
     assert_eq!(api_error.message, "Internal database error");
+
+    // Group error
+    let error = ErrorEn::GroupError(GrpError::FirewallFail);
+    let api_error = error.as_api_error();
+    assert_eq!(api_error.code, 500);
+    assert_eq!(api_error.kind, "firewall_fail");
 }
